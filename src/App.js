b01982ce8b02906107d382a78cbc6cb8f0d6c5f9
zheniagnedchik <zhenyagnedchik@gmail.com>
2021-01-05 13:29:32 +0300

    add reg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import QuizMain from "./Components/Quiz/QuizMain";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Reg from './components/Reg/Reg'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <QuizMain />
-        </Switch>
-      </Router>
+      <Reg/>
     </div>
   );
 }
