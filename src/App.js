0b2f704da9f07ea4c84fd547e3c8f4ae5f3b4d13
zheniagnedchik <zhenyagnedchik@gmail.com>
2021-01-06 18:26:08 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import QuizMain from "./components/Quiz/QuizMain";
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-         
-         
-          <Route path="/" component={QuizMain}></Route>
-         
-        </Switch>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
